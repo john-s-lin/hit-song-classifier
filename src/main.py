bc83fff420fc3766110682bce1ef00365a0bb7bc
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2023-02-26 18:07:40 -0500

    test: pass linter flow

--- src/main.py
+++ src/main.py
@@ -1,22 +1,15 @@
 class TestLinter:
     def __init__(self) -> None:
         self.x = 10
-
-
-
-        self.emoji =             "🐍"
+        self.emoji = "🐍"
 
     def about_me(self) -> None:
-
-
-
-
         print(f"My value is {self.x}. I am testing the linter {self.emoji}")
 
 
 def main():
     test = TestLinter()
-    test. about_me()
+    test.about_me()
 
 
 if __name__ == "__main__":
