4f9742693767d6b59e58dde7c3ab483596770e28
Sephora <seph156@gmail.com>
2023-03-12 20:28:30 -0400

    modified to take care of some linting issues

--- src/billboard_preprocessor.py
+++ src/billboard_preprocessor.py
@@ -8,9 +8,11 @@ from pyspark.sql.functions import col, min, floor
 from pyspark.sql.types import IntegerType, DateType
 
 '''
-Billboard 100 dataset kaggle info. 
+Billboard 100 dataset kaggle info.
 This dataset includes the hot 100 songs from 1958 to 2021
-DOI citation: Dhruvil Dave. (2021). <i>Billboard "The Hot 100" Songs</i> [Data set]. Kaggle. https://doi.org/10.34740/KAGGLE/DS/1211465
+DOI citation:
+    Dhruvil Dave. (2021). <i>Billboard "The Hot 100" Songs</i>
+    [Data set]. Kaggle. https://doi.org/10.34740/KAGGLE/DS/1211465
 '''
 kaggle_path = 'dhruvildave/billboard-the-hot-100-songs'
 kaggle_filename = 'charts.csv'
@@ -19,6 +21,7 @@ kaggle_filename = 'charts.csv'
 root_dir = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
 temp_dir = os.path.join(root_dir, 'data', 'temp', '')
 
+
 # kaggle API fetch and extract dataset
 def download_kaggle_dataset(k_path: str, k_filename: str, directory: str):
     api = KaggleApi()
@@ -26,12 +29,18 @@ def download_kaggle_dataset(k_path: str, k_filename: str, directory: str):
 
     print(f'Downloading {k_filename} from the Kaggle API at {k_path}.')
     api.dataset_download_file(k_path, file_name=k_filename, path=directory)
-    with ZipFile(f'{os.path.join(directory, k_filename)}.zip', 'r') as zip_object:
+    zip_path = f'{os.path.join(directory, k_filename)}.zip'
+    with ZipFile(zip_path, 'r') as zip_object:
         zip_object.extractall(directory)
+
 
 # process the downloaded csv data, taking the top ranking for all years before 2011.
 def process_csv(filename: str) -> DataFrame:
-    spark = SparkSession.builder.appName('Hot-Song-classifier').getOrCreate()
+    spark = (
+        SparkSession.builder
+        .appName('Hot-Song-classifier')
+        .getOrCreate()
+    )
     
     df = spark.read.csv(filename, header=True, mode='DROPMALFORMED')
     df = df.withColumn('rank', df['rank'].cast(IntegerType()))
@@ -43,6 +52,7 @@ def process_csv(filename: str) -> DataFrame:
     )
     return max_rank_df
 
+
 # classify top rankings into buckets ranging from 0 to 9
 def classify_rankings(df: DataFrame) -> DataFrame:
     classified_df = df.withColumn('class_f', ((df.top_rank-1)/10))
@@ -52,10 +62,12 @@ def classify_rankings(df: DataFrame) -> DataFrame:
     )
     return classified_df
 
+
 # removes a directory and all content
 def remove_directory(directory_path):
     print(f'removing directory {directory_path}')
     shutil.rmtree(directory_path)
+
 
 # writes a dataframe to a csv file at the given path
 def write_csv(df: DataFrame, write_path: str):
@@ -68,9 +80,10 @@ def write_csv(df: DataFrame, write_path: str):
     count = 1
     for file in os.listdir(write_path):
         if file.endswith(".csv"):
-            os.rename(os.path.join(write_path, file), os.path.join(root_dir, 'data', f'charts{count}.csv'))
+            path = os.path.join(root_dir, 'data', f'charts{count}.csv')
+            os.rename(os.path.join(write_path, file), path)
             count += 1
-    #remove write_path directory and all contained files
+    # remove write_path directory and all contained files
     remove_directory(write_path)
 
 
@@ -79,5 +92,7 @@ download_kaggle_dataset(kaggle_path, kaggle_filename, temp_dir)
 processed_data = process_csv(os.path.join(temp_dir, kaggle_filename))
 classified_rankings = classify_rankings(processed_data)
 
-write_csv(classified_rankings, os.path.join(root_dir, 'data', 'classified_songs'))
+write_path = os.path.join(root_dir, 'data', 'classified_songs')
+write_csv(classified_rankings, write_path)
+
 remove_directory(temp_dir)
