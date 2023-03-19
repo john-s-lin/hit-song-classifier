35df6bf64a34af2c388b04d57268e0c793606c08
William-Chittavong <chizzycaps@yahoo.com>
2023-03-19 19:55:20 -0400

    changes to million_song.py
    moved functions, removed the print statements, removed the extra csv folder.

--- src/million_song.py
+++ src/million_song.py
@@ -4,32 +4,32 @@ import tarfile
 import dask.dataframe as dd
 import glob
 
+#Define the download function
+def download(url: str) -> None:
+    """Download file from url
+
+    Args:
+        url (str): url to download file from
+    """
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        filename = os.path.basename(url)
+        filepath = f"data/{filename}"
+        with open(filepath, "wb") as f:
+            f.write(response.raw.read())
+def extract_file(filepath: str) -> None:
+    """Extract file from tar.gz"""
+    print(f"Extracting {filepath}...")
+    tar = tarfile.open(filepath, "r:gz")
+    tar.extractall(path="data")
+    tar.close()
+
+def process(df, columns_to_keep):
+    df_filtered = df.loc[:, columns_to_keep]
+    df_filtered = df_filtered.dropna(how='any')
+    return df_filtered
+
 def main():
-    #Define the download function
-    def download(url: str) -> None:
-        """Download file from url
-
-        Args:
-            url (str): url to download file from
-        """
-        response = requests.get(url, stream=True)
-        if response.status_code == 200:
-            filename = os.path.basename(url)
-            filepath = f"data/{filename}"
-            with open(filepath, "wb") as f:
-                f.write(response.raw.read())
-    def extract_file(filepath: str) -> None:
-        """Extract file from tar.gz"""
-        print(f"Extracting {filepath}...")
-        tar = tarfile.open(filepath, "r:gz")
-        tar.extractall(path="data")
-        tar.close()
-
-    def process(df, columns_to_keep):
-        df_filtered = df.loc[:, columns_to_keep]
-        df_filtered = df_filtered.dropna(how='any')
-        return df_filtered
-
 
     features = [
         "artist_name",
@@ -46,18 +46,17 @@ def main():
     ]
     parent_dir = ".\src"
     h5_files = list(glob.glob(parent_dir + "**/**/*.h5", recursive=True))
+    
     analysis_songs = dd.read_hdf(h5_files, key='analysis/songs')
     meta_songs = dd.read_hdf(h5_files, key='metadata/songs')
     music_songs = dd.read_hdf(h5_files, key='musicbrainz/songs')
-
-   
+    
     merged = dd.concat([analysis_songs, meta_songs, music_songs], axis=1)
     merged = merged.repartition(npartitions=10)
     filtered = process(merged,features)
-    print(filtered.head(npartitions=5,n=5))
+   
     filtered.to_csv('./data/million_songs.csv', index=False,single_file=True)
-    # print("merged:")
-    # print(merged.head(n=10))
+  
 
 
 if __name__ == "__main__":
