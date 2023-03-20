da3dd0cc808e859301d4f43ebdb68619a1255528
William-Chittavong <chizzycaps@yahoo.com>
2023-03-19 20:28:21 -0400

    Fixed docstring, Placed download and extract file steps. Formatted using black.

--- src/million_song.py
+++ src/million_song.py
@@ -4,7 +4,7 @@ import tarfile
 import dask.dataframe as dd
 import glob
 
-#Define the download function
+
 def download(url: str) -> None:
     """Download file from url
 
@@ -17,6 +17,8 @@ def download(url: str) -> None:
         filepath = f"data/{filename}"
         with open(filepath, "wb") as f:
             f.write(response.raw.read())
+
+
 def extract_file(filepath: str) -> None:
     """Extract file from tar.gz"""
     print(f"Extracting {filepath}...")
@@ -24,13 +26,27 @@ def extract_file(filepath: str) -> None:
     tar.extractall(path="data")
     tar.close()
 
-def process(df, columns_to_keep):
+
+def process(df: dd, columns_to_keep: list[str]) -> dd:
+    """The filtered Dask DataFrame with specified columns and no NaN values.
+    Args:
+        dd: dask dataframe
+        columns_to_keep: features that won't be removed from the input dask dataframe.
+    Returns: dask dataframe with only specified columns and null values removed.
+    """
     df_filtered = df.loc[:, columns_to_keep]
-    df_filtered = df_filtered.dropna(how='any')
+    df_filtered = df_filtered.dropna(how="any")
     return df_filtered
 
-def main():
 
+def main():
+    SUBSET_URL = "http://labrosa.ee.columbia.edu/~dpwe/tmp/millionsongsubset.tar.gz"
+    if not os.path.exists("data"):
+        os.mkdir("data")
+    if not os.path.exists("data/millionsongsubset.tar.gz"):
+        download(SUBSET_URL)
+    if not os.path.exists("data/MillionSongSubset"):
+        extract_file("data/millionsongsubset.tar.gz")
     features = [
         "artist_name",
         "title",
@@ -46,17 +62,16 @@ def main():
     ]
     parent_dir = ".\src"
     h5_files = list(glob.glob(parent_dir + "**/**/*.h5", recursive=True))
-    
-    analysis_songs = dd.read_hdf(h5_files, key='analysis/songs')
-    meta_songs = dd.read_hdf(h5_files, key='metadata/songs')
-    music_songs = dd.read_hdf(h5_files, key='musicbrainz/songs')
-    
+
+    analysis_songs = dd.read_hdf(h5_files, key="analysis/songs")
+    meta_songs = dd.read_hdf(h5_files, key="metadata/songs")
+    music_songs = dd.read_hdf(h5_files, key="musicbrainz/songs")
+
     merged = dd.concat([analysis_songs, meta_songs, music_songs], axis=1)
     merged = merged.repartition(npartitions=10)
-    filtered = process(merged,features)
-   
-    filtered.to_csv('./data/million_songs.csv', index=False,single_file=True)
-  
+    filtered = process(merged, features)
+
+    filtered.to_csv("./data/million_songs.csv", index=False, single_file=True)
 
 
 if __name__ == "__main__":
