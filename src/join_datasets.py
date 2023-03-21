1e94d689cdb1de5a45b4a857e00d1ba1666d5e4e
smarkandu <s_markan@hotmail.com>
2023-03-20 23:46:10 -0400

    feat: Put common "generic" pyspark functions into its own file, now writes .csv file for the joins, .csv file included

--- src/join_datasets.py
+++ src/join_datasets.py
@@ -1,63 +1,21 @@
-import csv
-import numpy as np
-import os
-import sys
-# Spark imports
-from pyspark.rdd import RDD
-from pyspark.sql import DataFrame
-from pyspark.sql import SparkSession
-from pyspark.sql import Row
-from pyspark.sql.functions import desc
-# Dask imports
-import dask.bag as db
-import dask.dataframe as df  # you can use Dask bags or dataframes
-from csv import reader
+from utilities.pyspark_utilities import write_csv, rename_csv, init_spark
 
 
-def init_spark():
-    spark = SparkSession \
-        .builder \
-        .appName("Python Spark SQL basic example") \
-        .config("spark.some.config.option", "some-value") \
-        .getOrCreate()
-    return spark
-
-
-# Useful functions to print RDDs and Dataframes.
-def toCSVLineRDD(rdd):
-    '''
-    This function convert an RDD or a DataFrame into a CSV string
-    '''
-    a = rdd.map(lambda row: ",".join([str(elt) for elt in row])) \
-        .reduce(lambda x, y: os.linesep.join([x, y]))
-    return a + os.linesep
-
-
-def toCSVLine(data):
-    '''
-    Convert an RDD or a DataFrame into a CSV string
-    '''
-    if isinstance(data, RDD):
-        return toCSVLineRDD(data)
-    elif isinstance(data, DataFrame):
-        return toCSVLineRDD(data.rdd)
-    return None
-
-
-def intersection_datasets():
-    spark = init_spark()
+def join_datasets():
+    spark = init_spark("join-datasets")
 
     df1 = spark.read.csv("./data/classified_billboard_songs1.csv", header=True, mode="DROPMALFORMED")
 
     df2 = spark.read.csv("./data/million_songs.csv", header=True, mode="DROPMALFORMED")
-    df2 = df2.withColumnRenamed("artist_name", "artist") \
-        .withColumnRenamed("title", "song")
+    df2 = df2.withColumnRenamed("title", "song")  \
+          .withColumnRenamed("artist_name", "artist") \
 
-    df3 = df1.join(df2, ['artist', 'song'])
-    return_value = toCSVLine(df3).replace("\r\n", "\n")
+    df3 = df1.join(df2, ['song', 'artist'])
+    # df3 = df1.join(df2, ['song']).orderBy('song')
 
-    # Write to CSV
-    df3.write.csv("./data/million_songs_billboard_join.csv")
+    return df3
 
 
-intersection_datasets()
+df_join = join_datasets()
+write_csv(df_join, ".\\data\\join_datasets.csv")
+rename_csv(".\\data\\join_datasets.csv", ".\\data", "join_datasets")
---
+++ src/utilities/pyspark_utilities.py
@@ -0,0 +1,48 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame
+import os
+import shutil
+
+
+def init_spark(app_name: str):
+    spark = SparkSession \
+        .builder \
+        .appName(app_name) \
+        .getOrCreate()
+    return spark
+
+
+def write_csv(df: DataFrame, write_path: str):
+    df.write.format("csv") \
+        .mode('overwrite') \
+        .option("header", "true") \
+        .save(write_path)
+
+
+def rename_csv(old_dir: str, new_dir, filename_prefix: str):
+    # rename file
+    count = 1
+    for file in os.listdir(old_dir):
+        if file.endswith(".csv"):
+            # Create "new" file name, using the count to make files unique
+            filename = f'{filename_prefix}{count}.csv'
+
+            # Create complete file paths for the old and new files
+            old_file_path = os.path.join(old_dir, file)
+            new_file_path = os.path.join(new_dir, filename)
+
+            # If "new" file already exists, remove it
+            if os.path.isfile(new_file_path):
+                os.remove(new_file_path)
+
+            # Use rename to move "old" file as "new" file
+            os.rename(old_file_path, new_file_path)
+            count += 1
+
+    # remove old directory and all contained files
+    remove_directory(old_dir)
+
+
+def remove_directory(directory_path: str):
+    print(f'removing directory {directory_path}')
+    shutil.rmtree(directory_path)
