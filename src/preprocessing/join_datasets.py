65ae7505f844e2e193d9f77b50249f10e8bdcfd1
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2023-03-30 12:47:45 -0400

    feat: add RFC and LR models in `pyspark`, RFC and SVM models in `sklearn` (#16)

---
+++ src/preprocessing/join_datasets.py
@@ -0,0 +1,22 @@
+from utils.pyspark_utilities import write_csv, rename_csv, init_spark
+
+
+def join_datasets():
+    spark = init_spark("join-datasets")
+
+    df1 = spark.read.csv(
+        "./data/classified_billboard_songs1.csv", header=True, mode="DROPMALFORMED"
+    )
+
+    df2 = spark.read.csv("./data/million_songs.csv", header=True, mode="DROPMALFORMED")
+    df2 = df2.withColumnRenamed("title", "song").withColumnRenamed(
+        "artist_name", "artist"
+    )
+    df3 = df1.join(df2, ["song", "artist"]).orderBy("song")
+
+    return df3
+
+
+df_join = join_datasets()
+write_csv(df_join, ".\\data\\join_datasets.csv")
+rename_csv(".\\data\\join_datasets.csv", ".\\data", "join_datasets")
